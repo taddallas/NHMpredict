86e1c4f144af89f1d6884a31de03b829caf8e1f5
taddallas <tdallas@uga.edu>
2016-12-16 21:51:55 -0800

    initial commit. long ways to go

---
+++ R/nhm_site.R
@@ -0,0 +1,20 @@
+#' Access 
+#'
+#' @param limit number of records to download (if NULL, set to max value = 5000)
+#' 
+#' @return output
+#'
+#'
+#'
+
+
+nhm_species <- function(limit=NULL){
+  url <- "http://data.nhm.ac.uk"
+  id <- '902f084d-ce3f-429f-a6a5-23162c73fdf7'
+  ckanr_setup(url = url)
+  packs <- package_show(id=id, as='table')$resources
+  dat <- ds_search(resource_id=packs$id[2], url=url, as='table', limit=limit)
+
+
+
+}
